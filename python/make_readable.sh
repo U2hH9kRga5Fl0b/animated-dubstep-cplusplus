9c2f09a03f58dd30f9b46db54699163535faa263
hallock <me@example.com>
2014-11-03 00:18:13 -0700

    added the distance calculator

---
+++ python/make_readable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sed -i 's/\[//g'                           directions/*
+sed -i 's/\]//g'                           directions/*
+sed -i 's/)//g'                            directions/*
+sed -i 's/(//g'                            directions/*
+sed -i 's/), (//g'                         directions/*
+sed -i 's/\([0-9]\), \([0-9-]\)/\1 \2/g'   directions/*
+
+
+
+
+
+
